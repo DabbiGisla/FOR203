b978a9b465d9650a49603f1a488fd567f7494702
DabbiGisla <davidgi264@nemi.tskoli.is>
2017-01-24 13:41:18 +0000

    Dæmi 1 Klárað

---
+++ git_verkefni.py
@@ -0,0 +1,10 @@
+#Git Verkefni
+#Davíð Þór Gíslason
+
+#Dæmi 1
+tala1 = int(input("Sláðu inn tölu: "))
+tala2 = int(input("Sláðu inn aðra tölu: "))
+plus = tala1+tala2
+sinnum = tala1*tala2
+print ("Tölurnar lagðar saman er samtals: " +str(plus))
+print ("Tölurnar margfaldaðar saman er samtals: " +str(sinnum))
