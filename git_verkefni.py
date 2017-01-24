446924a7833be86d0c1b1d703e39a3a83a3c9536
DabbiGisla <davidgi264@nemi.tskoli.is>
2017-01-24 14:19:04 +0000

    Dæmi 3 Klárað

--- git_verkefni.py
+++ git_verkefni.py
@@ -13,3 +13,17 @@ print ("Tölurnar margfaldaðar saman er samtals: " +str(sinnum))
 fornafn = input("Hvað er fornafnið þitt? ")
 eftirnafn = input("Hvað er eftirnafnið þitt? ")
 print ("Halló "+fornafn+" "+ eftirnafn)
+
+#Dæmi 3
+litill = 0
+stor = 0
+litillstor = 0
+texti = input("Sláðu inn texta")
+for i in range(len(texti)):
+    if texti[i].isupper():
+       stor += 1
+       if texti[i+1].islower():
+           litillstor += 1
+    elif texti[i].islower():
+        litill += 1
+print ("Í þessum texta eru "+str(stor)+" hástafir, "+str(litill)+" lágstafir og "+str(litillstor)+" lágstafir koma strax á eftir hástaf")
